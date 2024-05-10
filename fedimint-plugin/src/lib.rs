5afbb5ba0ec986d27308dd093bfa4140f955a756
Ethan Tuttle <tuttle.ethan@protonmail.com>
2024-05-09 21:56:14 -0400

    feat: shell fedimint-plugin

--- fedimint-plugin/src/lib.rs
+++ fedimint-plugin/src/lib.rs
@@ -2,16 +2,24 @@ use tauri::{plugin::{Builder, TauriPlugin}, Runtime};
 
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
   Builder::new("fedimint")
+    .setup(|app, plugin| {
+        // setup client data_dir
+        // set out guardian peerId
+        // password for authentication (should not be here?)
+        // 
+        Ok(())
+    })
     .build()
 }
 
+
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        // let result = add(2, 2);
+        // assert_eq!(result, 4);
     }
 }
